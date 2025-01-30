ce4e4a415303c36468d1ef8b096f290e02d93990
DEV-EdCS <edlan.carvalho15@gmail.com>
2025-01-30 01:31:49 -0300

    Estudos

---
+++ Estudo 3/logica-js-projeto_inicial/app.js
@@ -0,0 +1,55 @@
+console.log ('Boas vindas!');
+let nome = 'Edlan';
+console.log (`Olá, ${nome}!`);
+alert (`Olá, ${nome}!`);
+
+// let linguagemFavorita = prompt ('Qual a linguagem de programação que você mais gosta?');
+// console.log (linguagemFavorita);
+
+// let valor1 = 7;
+// let valor2 = 4;
+// let resultado = (valor1 - valor2);
+// console.log (`A diferença entre ${valor1} e ${valor2} é igual a ${resultado}`);
+
+// let idade = prompt('Qual sua idade?');
+// if (idade >= 18){
+//   console.log('O usuário é maior de idade!'); 
+// } else {
+//   console.log('O usuário é menor de idade!')
+// };
+
+// let numero = prompt('Digite qualquer valor numérico!')
+// if (numero > 0){
+//   alert('O número digitado é positivo!'); 
+// } else if (numero < 0) {
+//   alert('O número digitado é negativo!');
+// } else {
+//   alert('O número digitado é zero!');
+// };
+
+// let contador = 1;
+// // Enquanto o contador for menor ou igual a 10, o loop continua
+// while (contador <= 10) {
+//   console.log(contador); // Exibe o número atual
+//   contador++; // Incrementa o contador
+// }
+
+// Crie uma variável "nota" e atribua um valor numérico a ela. Use if-else para determinar se a nota é maior ou igual a 7 e exiba "Aprovado" ou "Reprovado" no console.
+// let nota = 9;
+// if (nota >= 7) {
+//   console.log ('Aprovado!')
+// } else {
+//   console.log ('Reprovado!')
+// };
+
+// Use o Math.random para gerar qualquer número aleatório e exiba esse número no console.
+let numeroAleatorio = Math.random();
+console.log(numeroAleatorio)
+
+// Use o Math.random para gerar um número inteiro entre 1 e 10 e exiba esse número no console.
+let numeroAleatorio2 = parseInt(Math.random()* 10) + 1;
+console.log(numeroAleatorio2)
+
+// Use o Math.random para gerar um número inteiro entre 1 e 1000 e exiba esse número no console.
+let numeroAleatorio3 = parseInt(Math.random()* 1000) + 1;
+console.log(numeroAleatorio3)
