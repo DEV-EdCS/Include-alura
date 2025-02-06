3ccc5bf2e55ab05cbcc72fb8606a68d0382db924
DEV-EdCS <edlan.carvalho15@gmail.com>
2025-02-05 22:56:47 -0300

    Estudo de função

--- Semana 2_Lógica de programação_explore funções e listas/js-curso-2-aula1/app.js
+++ Semana 2_Lógica de programação_explore funções e listas/js-curso-2-aula1/app.js
@@ -1,9 +1,35 @@
-let titulo = document.querySelector('h1'); // Recolhe e seleciona a tag h1 do html
-titulo.innerHTML = 'Jogo do número secreto'; // Insere no h1 selecionado a frase
+// ANTES
+/* let titulo = document.querySelector('h1'); // Recolhe e seleciona a tag h1 do html
+titulo.innerHTML = 'Jogo do número secreto'; // Insere no h1 selecionado a frase */
 
-let paragrafo = document.querySelector('p'); // Recolhe e seleciona a tag p do html
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10'; // Insere no p selecionado a frase
+// ANTES
+/* let paragrafo = document.querySelector('p'); // Recolhe e seleciona a tag p do html
+paragrafo.innerHTML = 'Escolha um número entre 1 e 10'; // Insere no p selecionado a frase */
+
+// TODAS APRESENTAM O SEGUINTE PADRÃO
+/* let campo = document.querySelector(tag)
+campo.innerHTML = texto */
+
+// DEPOIS
+
+let numeroSecreto = gerarNumeroAleatorio(); // Variável para guardar o valor da função "gerarNumeroAleatorio"
+
+// Função para executar o padrão de tag do HTML e exibir o texto 
+function exibirTextoNaTela(tag, texto) {
+  let campo = document.querySelector(tag);
+  campo.innerHTML = texto;
+}
+
+
+exibirTextoNaTela('h1', 'Jogo do número secreto'); // Substitui a tag da função pelo h1 do HTML
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10'); // Substitui a tag da função pelo h1 do HTML
 
 function verificarChute() { // Cria a função para o botão
-    console.log('O botão foi clicado!') // Exibe no console quantas vezes o botão foi clicado
-  }
+    let chute = document.querySelector('input').value; // Atribui o valor do input à variável "chute"
+    console.log(chute == numeroSecreto); // Compara o valor do chute com o numero secreto aleatorio, e devolve verdadeiro ou falso
+}
+
+// Função para criar um número aleatório
+function gerarNumeroAleatorio() {
+  return parseInt(Math.random() * 10 + 1); // utiliza o Math.randon para criar o número aleatorio e o parseInt para transformar ele em inteiro. Depois acrescenta o retun para ele guardar esse número e devolver quando chamar a função "gerarNumeroAleatorio"
+}
