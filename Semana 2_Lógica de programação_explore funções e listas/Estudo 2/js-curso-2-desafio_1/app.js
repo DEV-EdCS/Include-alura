3a12d8740c22b963b469aaff7a7c174013470737
DEV-EdCS <edlan.carvalho15@gmail.com>
2025-02-05 23:10:09 -0300

    Criar funções

---
+++ Semana 2_Lógica de programação_explore funções e listas/Estudo 2/js-curso-2-desafio_1/app.js
@@ -0,0 +1,24 @@
+// Criar uma função que exibe "Olá, mundo!" no console.
+let olaMundo = exibirHelloWord();
+
+function exibirHelloWord(){
+   return console.log('Olá, Mundo!');
+}
+
+// Criar uma função que recebe um nome como parâmetro e exibe "Olá, [nome]!" no console.
+let nome = ('Mundo');
+
+function receberNome(nome) {
+   return console.log(`Olá ${nome}!`);
+}
+
+console.log(receberNome());
+
+// Criar uma função que recebe um número como parâmetro e retorna o dobro desse número.
+
+
+// Criar uma função que recebe três números como parâmetros e retorna a média deles.
+
+// Criar uma função que recebe dois números como parâmetros e retorna o maior deles.
+
+// Criar uma função que recebe um número como parâmetro e retorna o resultado da multiplicação desse número por ele mesmo
